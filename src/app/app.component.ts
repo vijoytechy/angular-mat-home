9c1c7a01e572804f59868e075b6d56be47cfe00d
vijoytechy <vijoytechy@gmail.com>
2023-08-31 21:30:44 +0300

    Angular Homes with standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  standalone: true,
+  template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
+  styleUrls: ['./app.component.scss'],
+  imports: [HomeComponent, RouterModule,]
 })
 export class AppComponent {
   title = 'matapp';
